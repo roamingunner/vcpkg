f1f3d1946ee4ae725bdaf7e16588371275429d3a
leo.qin <103922926@qq.com>
2021-06-29 11:21:58 +0800

    add linaro triplet and toolchain as custom toolchain example

---
+++ triplets/community/arm-linaro.cmake
@@ -0,0 +1,7 @@
+set(VCPKG_TARGET_ARCHITECTURE arm)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../../scripts/toolchains/arm-linaro-toolchain.cmake")
+
